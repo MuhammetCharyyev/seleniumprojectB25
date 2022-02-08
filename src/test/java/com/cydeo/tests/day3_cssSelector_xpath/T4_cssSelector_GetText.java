af68a65d0f10aa2fbdaf275d38d174f22e147c91
Muhammet <muhammedabuchary@gmail.com>
2022-02-08 15:02:55 -0600

    Day3_cssSelector_xpath

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T4_cssSelector_GetText.java
@@ -0,0 +1,46 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T4_cssSelector_GetText {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        //getting from method we created in Utility package
+        driver.manage().window().maximize();
+
+        //2- Go to: https://login1.nextbasecrm.com/?forgot_password=yes
+        driver.get("https://login1.nextbasecrm.com/?forgot_password=yes");
+
+//3- Verify “Reset password” button text is as expected:
+//Expected: Reset password
+       // onclick="BX.addClass(this, 'wait');
+        WebElement resetPassword = driver.findElement(By.cssSelector("button[value='Reset password']"));
+
+        String expectedResetText = "Reset password";
+        String actualResetText = resetPassword.getAttribute("value");
+        //signInButton.getAttribute ->because "Reset password" text is inside the attribute 'value'
+
+        System.out.println("actualResetText = " + actualResetText);
+        //printed out -> actualButtonText = Log In
+
+        if(actualResetText.equals(expectedResetText)){
+            System.out.println("reset verification passed");
+        }else{
+            System.out.println("rest verification failed");
+        }
+    }
+
+
+}
+/*
+TC #4: NextBaseCRM, locators, getText(), getAttribute() practice
+1- Open a chrome browser
+2- Go to: https://login1.nextbasecrm.com/?forgot_password=yes
+3- Verify “Reset password” button text is as expected:
+Expected: Reset password
+ */
