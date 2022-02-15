4abe3ba082359cf102eddcf93ed4649aea5c8495
Muhammet <muhammedabuchary@gmail.com>
2022-02-15 14:14:59 -0600

    Day5_TestNG_Class

--- src/test/java/com/cydeo/tests/day5_dynamicWebElem_testNG_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_dynamicWebElem_testNG_dropdowns/TestNG_Intro.java
@@ -1,15 +1,38 @@
 package com.cydeo.tests.day5_dynamicWebElem_testNG_dropdowns;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
 
-    @Test //make it runnable, replace main() method
+    @BeforeClass //executed once before everything in class
+    public void setupMethod() {
+        System.out.println("-----> BeforeClass is running!");
+
+    }
+
+    @AfterClass //executed once after everything in class
+    public void tearDownClass(){
+        System.out.println("-----> AfterClass is running!");
+
+    }
+
+    @BeforeMethod //execute everything before each method/@Test in class
+    public void setUpMethod(){
+        System.err.println("-->BeforeMethod is running<--");
+    }
+
+   @AfterMethod //execute everything after each method/@Test in class
+    public void tearDownMethod(){
+        System.err.println("-->AfterMethod is running<--");
+    }
+
+    @Test (priority = 1)//make it runnable, replace main() method
+    // priority make execution in line
     public void test1(){
         System.out.println("test1 is running");
     }
 
-    @Test
+    @Test (priority = 2) //define the priority to execute
     public void test2(){
         System.out.println("test2 is running");
     }
