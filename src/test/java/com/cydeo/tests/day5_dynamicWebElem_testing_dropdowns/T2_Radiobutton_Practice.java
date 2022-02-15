0aae5caeb4971d999ab592cb079a09f87677cbb3
Muhammet <muhammedabuchary@gmail.com>
2022-02-14 22:17:01 -0600

    Day5_Xpath_practice

---
+++ src/test/java/com/cydeo/tests/day5_dynamicWebElem_testing_dropdowns/T2_Radiobutton_Practice.java
@@ -0,0 +1,39 @@
+package com.cydeo.tests.day5_dynamicWebElem_testing_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class T2_Radiobutton_Practice {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        //getting from method we created in Utility package
+        driver.manage().window().maximize();
+
+        //providing extra wait time before it throws NoSuchElement Exception
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // 2- Go to https://practice.cydeo.com/radio_buttons
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        //3. Click to “Hockey” radio button
+        WebElement hockeyRadioBtn = driver.findElement(By.xpath("//input[@id='hockey']"));
+        hockeyRadioBtn.click();
+
+        //4. Verify “Hockey” radio button is selected after clicking.
+        System.out.println("hockeyRadioBtn.isSelected() = " + hockeyRadioBtn.isSelected());
+
+    }
+}
+/**
+ * TC #2: Radiobutton handling
+ * 1. Open Chrome browser
+ * 2. Go to https://practice.cydeo.com/radio_buttons
+ * 3. Click to “Hockey” radio button
+ * 4. Verify “Hockey” radio button is selected after clicking.
+ */
