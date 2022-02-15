4c60bd7b6947b16772859518e752cf3b3570a250
Muhammet <muhammedabuchary@gmail.com>
2022-02-15 13:29:17 -0600

    Day5_TestNG_Class

--- src/test/java/com/cydeo/tests/day5_dynamicWebElem_testNG_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_dynamicWebElem_testNG_dropdowns/TestNG_Intro.java
@@ -1,4 +1,12 @@
 package com.cydeo.tests.day5_dynamicWebElem_testNG_dropdowns;
 
+import org.testng.annotations.Test;
+
 public class TestNG_Intro {
+
+    @Test //make it runnable, replace main() method
+    public void test1(){
+        System.out.println("test1 is running");
+    }
+
 }
