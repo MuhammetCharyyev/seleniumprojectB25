e1a1d8830fc479dd2dd4f4803e888b5fba8fa25e
Muhammet <muhammedabuchary@gmail.com>
2022-02-18 10:36:19 -0600

    Day6_T5_State_Dropdowns

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,16 +24,38 @@ public class DropdownPractices {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-
         driver.get("https://practice.cydeo.com/dropdown");
     }
 
     @Test
-    public void dropdown_task5(){
+    public void dropdown_task5() {
+//3. Select Illinois
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
 
+        stateDropdown.selectByVisibleText("Illinois"); //selected by text "Illinois"
 
+        //4. Select Virginia
 
+       stateDropdown.selectByValue("VA"); //selected by value 'VA'
+
+        //5. Select California
+
+      stateDropdown.selectByIndex(5);//select by index num in dropdown menu
+
+     //   6. Verify final selected option is California.
+        String expectedOptionText = "California";
+        String actualOptionText = stateDropdown.getFirstSelectedOption().getText();
+        //check if the first text is matched
+
+        Assert.assertEquals(actualOptionText, expectedOptionText);
     }
+
+    @AfterMethod
+
+    public void closeDriver(){
+        driver.close();
+    }
+
 }
 /*
 TC #5: Selecting state from State dropdown and verifying result
