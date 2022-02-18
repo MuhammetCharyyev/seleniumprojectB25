ce8ca029d2b9640ef498e3c84bdfc5840035adff
Muhammet <muhammedabuchary@gmail.com>
2022-02-18 14:07:33 -0600

    Day6_T1_Alerts_Practice

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alerts_Practices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alerts_Practices.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -23,15 +27,34 @@ public class T1_Alerts_Practices {
         driver.get("http://practice.cydeo.com/javascript_alerts");
     }
 
-    //3. Click to “Click for JS Alert” button
 
     @Test
-    public void clickAlertBtn(){
+    public void alert_test1(){
+//3. Click to “Click for JS Alert” button
+        WebElement infoAlertBtn = driver.findElement(By.xpath("//button[.='Click for JS Alert']"));
+        infoAlertBtn.click();
+
+        //4. Click to OK button from the alert
+        //to be able to click Alert ok button we need switch driver's focus to Alert itself
+
+        Alert alert = driver.switchTo().alert();//switching to Alert itself
+        alert.accept();
+//accepting the Alert message because we are not able to inspect this pop up Alert message
+
+        //5. Verify “You successfully clicked an alert” text is displayed
+        WebElement resultAlertOkBtn = driver.findElement(By.xpath("//p[@id='result']"));
+
+        Assert.assertTrue(resultAlertOkBtn.isDisplayed(), "Result text is not displayed");
+        //assertTrue will return with warning text if only it's 'false'
+
+        String expectedText = "You successfully clicked an alert";
+        String actualText = resultAlertOkBtn.getText();
+
+        Assert.assertEquals(actualText, expectedText, "Actual text result is not as expected!!!");
+        //will return with message if actual!=expected
 
 
     }
-
-
 
 }
 /**
