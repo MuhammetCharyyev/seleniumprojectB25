69089bebfefa45b61fcf48d9b2969656e7bd2a0d
Muhammet <muhammedabuchary@gmail.com>
2022-02-22 11:07:37 -0600

    Day7_T1_WindowHandling_Practice

--- src/test/java/com/cydeo/tests/day7_Webtables_Utilities_JavaFaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_Webtables_Utilities_JavaFaker/T1_WindowHandling.java
@@ -3,9 +3,11 @@ package com.cydeo.tests.day7_Webtables_Utilities_JavaFaker;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
@@ -19,7 +21,8 @@ public class T1_WindowHandling {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+// * 2. Go to Go to : https://www.amazon.com/
+        driver.get("Go to : https://www.amazon.com/");
 
     }
 
@@ -27,14 +30,32 @@ public class T1_WindowHandling {
 
     public void window_handling_test(){
 
-// * 2. Go to Go to : https://www.amazon.com/
-        driver.get("Go to : https://www.amazon.com/");
-
        // 3. Copy paste the lines from below into your class
-        ((JavascriptExecutor) driver).executeScript("window.open('http://google.com/%27,%27_blank%27);%22);");
-        ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com/%27,%27_blank%27);%22);");
-        ((JavascriptExecutor) driver).executeScript("window.open('http://facebook.com/%27,%27_blank%27);%22);");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://google.com/%27,%27_blank%27);%22);");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com/%27,%27_blank%27);%22);");
+        ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com/%27,%27_blank%27);%22);");
 
+        // 4. Create a logic to switch to the tab where Etsy.com is open
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+
+            driver.switchTo().window(each);
+
+            System.out.println("Current Url is : " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains("etsy")){
+                break;
+            }
+
+        }
+
+        //5. Assert: Title contains “Etsy”
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
 
     }
 
