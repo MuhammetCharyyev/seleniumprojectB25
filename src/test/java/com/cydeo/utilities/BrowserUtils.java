f5e451b1721f9fcb2103fbc00c9bfe3281b9f0de
Muhammet <muhammedabuchary@gmail.com>
2022-02-22 14:38:03 -0600

    Day7_BrowserUtils_Class

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -11,6 +11,11 @@ import java.util.Set;
 
 public class BrowserUtils {
 
+    /*
+    This method will accept int (in seconds) and execute Thread.sleep
+    for given duration
+     */
+
     public void sleep(int second) {
         second *= 1000;
         try {
@@ -18,7 +23,19 @@ public class BrowserUtils {
         } catch (InterruptedException e) {
         }
     }
+    /*
+    1. Create a new class called BrowserUtils
+    2. Create a method to make Task1 logic re-usable
+    3. When method is called, it should switch window and verify title.
 
+
+    Method info:
+    • Name: switchWindowAndVerify
+    • Return type: void
+    • Arg1: WebDriver
+    • Arg2: String expectedInUrl
+    • Arg3: String expectedTitle
+     */
     public static void switchWindowAndVerify (WebDriver driver, String expectedInURL, String expectedTitle){
 
         Set<String> allWindowsHandles = driver.getWindowHandles();
@@ -40,22 +57,14 @@ public class BrowserUtils {
 
     }
 
+    public static void verifyTitle (WebDriver driver, String expectedTitle){
+
+        Assert.assertEquals(driver.getTitle(), expectedTitle);
+
+    }
+
+
+
 }
-/*
-    This method will accept int (in seconds) and execute Thread.sleep
-    for given duration
-     */
-
-/*
-1. Create a new class called BrowserUtils
-2. Create a method to make Task1 logic re-usable
-3. When method is called, it should switch window and verify title.
 
 
-Method info:
-• Name: switchWindowAndVerify
-• Return type: void
-• Arg1: WebDriver
-• Arg2: String expectedInUrl
-• Arg3: String expectedTitle
- */
