2daf80e5124b62a469828bde7972c89db85066ed
Muhammet <muhammedabuchary@gmail.com>
2022-02-25 14:22:55 -0600

    Day8_LearningProperties

---
+++ src/test/java/com/cydeo/tests/day8_Properties_Config_Reader/ReadingProperties.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day8_Properties_Config_Reader;
+
+import org.testng.annotations.Test;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_test() {
+
+
+
+    }
+}
