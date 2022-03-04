2d59bcffce988efd53d446512d3abd9220dd2694
Muhammet <muhammedabuchary@gmail.com>
2022-03-04 10:40:15 -0600

    Day9_JavaFaker_Practice

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFaker_Practice.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class JavaFaker_Practice {
+
+    @Test
+    public void test1() {
+
+
+
+    }
+}
