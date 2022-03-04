89facfa30f657e050cb27bf6806ff290bb80bea0
Muhammet <muhammedabuchary@gmail.com>
2022-03-04 15:17:03 -0600

    Day9_T4_ConfigPractice_Adding_DriverUtility

--- src/test/java/com/cydeo/tests/day8_Properties_Config_Reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_Properties_Config_Reader/T4_Config_Practice.java
@@ -1,18 +1,15 @@
 package com.cydeo.tests.day8_Properties_Config_Reader;
 
 import com.cydeo.utilities.ConfigurationReader;
-import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
 public class T4_Config_Practice {
+/* comment below out for a while to try 'Driver' from Utility:
 
     public WebDriver driver;
 
@@ -26,14 +23,21 @@ public class T4_Config_Practice {
         driver = WebDriverFactory.getDriver(browser);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://google.com");
+
 
     }
 
+ */
+
     @Test
     public void google_search_test() {
+        Driver.getDriver().get("https://google.com");
+        //Driver.getDriver() called from 'Driver' Utility instead of just 'driver'
+
 // * 3- Write “apple” in search box
-        WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
+        //Driver.getDriver() called from 'Driver' Utility instead of just 'driver'
+
         googleSearchBox.sendKeys//searchValue is called from config properties
                 (ConfigurationReader.getProperty("searchValue")+ Keys.ENTER);
 
@@ -43,11 +47,10 @@ public class T4_Config_Practice {
         String expectedTitle = ConfigurationReader.getProperty("searchValue")+ " - Google Search";
         //we add here config properties option to have possibility
         // to change easily 'value' for searchValue in our config.prop file
-        String actualTitle = driver.getTitle();
+        String actualTitle =Driver.getDriver().getTitle();
+        //Driver.getDriver() called from 'Driver' Utility instead of just 'driver'
 
         Assert.assertEquals(actualTitle, expectedTitle);
-
-
     }
 }
 /**
