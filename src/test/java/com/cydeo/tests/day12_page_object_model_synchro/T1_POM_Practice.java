7a0f1776b2107384372889e5075bad39b1874345
Muhammet <muhammedabuchary@gmail.com>
2022-03-11 13:18:51 -0600

    Day12_T1_ErrorMess_Practice

--- src/test/java/com/cydeo/tests/day12_page_object_model_synchro/T1_POM_Practice.java
+++ src/test/java/com/cydeo/tests/day12_page_object_model_synchro/T1_POM_Practice.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 public class T1_POM_Practice {
 
+    LibraryLoginPage libraryLoginPage;
     @Test
     public void required_field_err_message_test() {
 
@@ -16,7 +17,7 @@ public class T1_POM_Practice {
 
        // 3- Do not enter any information
         // 4- Click to “Sign in” button
-        LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
+        libraryLoginPage = new LibraryLoginPage();
         //object from 'LibraryLoginPage'
 
         libraryLoginPage.signInButton.click();
@@ -28,6 +29,29 @@ public class T1_POM_Practice {
         Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
         //error method from 'LibraryLoginPage'
 
+        Driver.closeDriver();//close current driver but not terminate whole test
+
+    }
+
+    @Test
+    public void invalid_email_error_test() {
+
+        // * 2- Go to: https://library1.cydeo.com/
+        Driver.getDriver().get("https://library1.cydeo.com/");
+
+        //3- Enter invalid email format
+        libraryLoginPage = new LibraryLoginPage();
+
+        libraryLoginPage.inputUsername.sendKeys("somethingwrong");
+
+        libraryLoginPage.signInButton.click();
+
+        //4- Verify expected error is displayed:
+        //Expected: Please enter a valid email address.
+        Assert.assertTrue(libraryLoginPage.enterValidEmailErrorMessage.isDisplayed());
+
+        Driver.closeDriver();
+
     }
 }
 /**
@@ -38,6 +62,17 @@ public class T1_POM_Practice {
  * 4- Click to “Sign in” button
  * 5- Verify expected error is displayed:
  * Expected: This field is required.
+ *
+ * NOTE: FOLLOW POM DESIGN PATTERN
+ */
+
+/**
+ * TC #2: Invalid email format error message test
+ * 1- Open a chrome browser
+ * 2- Go to: https://library1.cydeo.com/
+ * 3- Enter invalid email format
+ * 4- Verify expected error is displayed:
+ * Expected: Please enter a valid email address.
  *
  * NOTE: FOLLOW POM DESIGN PATTERN
  */
