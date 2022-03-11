a7add88903e37d3af5e3f14377869a46f351054f
Muhammet <muhammedabuchary@gmail.com>
2022-03-11 15:52:50 -0600

    Day12_DynamicLoad1Page

---
+++ src/test/java/com/cydeo/pages/DynamicLoad1Page.java
@@ -0,0 +1,36 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DynamicLoad1Page {
+
+    public DynamicLoad1Page(){
+        //initElement will create connection between the current driver session(instance)
+        //and the object of current class
+        PageFactory.initElements(Driver.getDriver(), this);
+        // from PageFactory library// 'this' referring to this class itself
+        //expression in () is saying "take 'Driver.getDriver()' and place to 'this' class"
+    }
+
+    @FindBy (xpath = "//button[.='Start']")//locate 'start' button
+    public WebElement startButton;
+
+    @FindBy(css = "div#loading")//locating 'loading' function
+    public WebElement loadingBar;
+
+    @FindBy(css = "#username")//locating 'username' field
+    public WebElement inputUsername;
+
+    @FindBy(css = "#pwd")//locating 'password' field
+    public WebElement inputPassword;
+
+    @FindBy (xpath = "//button[.='Sumbit']")//locate 'submit' button
+    public WebElement submitButton;
+
+    @FindBy(id = "flash")//locating 'error' message
+    public WebElement errorMessage;
+
+}
